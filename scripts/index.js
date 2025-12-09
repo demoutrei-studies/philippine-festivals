888d78edaad66125405bd7eb7f7daaee4f9d6166
demoutrei-work <works.demoutrei@gmail.com>
2025-12-10 03:50:12 +0800

    Added CAR festival: Bodong Festival, and Kawayan Festival

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,51 @@ const navigation_button_image = document.getElementById("navigation-button-image
 const regions = document.querySelectorAll("[region]");
 
 
+function fetchJsonData() {
+  fetch('./data.json')
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      for (const region in data) {
+        section_content = document.querySelector(`[region="${region}"]`).querySelector(".content");
+        for (const i in data[region]) {
+          festival = data[region][i];
+          let paragraphs = [];
+          for (const i in festival["paragraphs"]) {
+            paragraphs.push(`<p>${festival["paragraphs"][i]}</p>`)
+          }
+          const template = `<div class="card">
+            <header>
+              <span class="name">${festival["name"]}</span>
+              <button class="collapse-button" onclick="display(this);">
+                <img class="image" src="./assets/collapse.png">
+              </button>
+            </header>
+            <main class="description">
+              <div class="info">
+                <div class="location">
+                  Location: <span class="data">${festival["location"]}</span>
+                </div>
+                <div class="date">
+                  Date: <span>${festival["date"]}</span>
+                </div>
+              </div>
+              <img class="banner" src="${festival["image_path"]}">
+              <div class="background">
+                ${paragraphs.join("\n")}
+              </div>
+            </main>
+          </div>`;
+          section_content.insertAdjacentHTML("beforeend", template);
+        }
+      }
+    })
+}
+
+fetchJsonData();
+
+
 function navigation() {
   if (navigation_button_image.classList.contains("opened")) {
     navigation_button_image.classList.remove("opened");
