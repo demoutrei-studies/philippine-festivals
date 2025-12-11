733b59650bdfd83346a46cf38566ba455752fc61
demoutrei-work <works.demoutrei@gmail.com>
2025-12-11 12:49:19 +0800

    List festivals

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
+const cards = document.querySelectorAll(".card");
 const clearSearchBarButton = document.querySelector("#clear-search-bar-button");
 const containerSearch = document.querySelector("#container-search");
+const contentElement = document.querySelector("#content");
 const searchBar = document.querySelector("#search-bar");
 const searchResultsList = document.querySelector("#search-results");
 
@@ -15,6 +17,31 @@ async function fetchData() {
   const response = await fetch('./data.json');
   return await response.json();
 }
+
+
+function listFestivals() {
+  contentElement.innerHTML = "";
+  fetchData().then(data => {
+    for (const group in data) {
+      for (const region in data[group]) {
+        for (let i in data[group][region]) {
+          festival = data[group][region][i];
+          const festivalCard = `<div class="card">
+            <div class="banner" style='background-image: url(\"${festival["image_path"]}\");'></div>
+            <div class="overview">
+              <span class="title">${festival["name"]}</span>
+              <p><b>Location</b>: ${festival["location"]}</p>
+              <p><b>Annual Date</b>: ${festival["date"]}</p>
+            </div>
+          </div>`;
+          contentElement.insertAdjacentHTML("beforeend", festivalCard);
+        }
+      }
+    }
+  })
+}
+
+listFestivals();
 
 
 function searchDisplay() {
